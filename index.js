e93140c31cc25a8e75030d7a30abd799723883b0
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-20 23:03:34 +0300

    completed assignment for string lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser+ " "+"!";
+console.log(welcomeMessage)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = 'Welcome, G !';
